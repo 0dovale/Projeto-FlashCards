426371ec19c280f6ae210c6e846ce717c1b5e638
dovale.xyz <84135635+0dovale@users.noreply.github.com>
2025-08-05 16:56:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 16?',
+    'A raiz quadrada de 16 é 4'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a raiz cúbica de 27?',
+    'A raiz cúbica de 27 é 3'
+)   
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
