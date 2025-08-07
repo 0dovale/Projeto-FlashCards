b07d58a3748174a9afc3d19777562fd3eb9a6199
dovale.xyz <84135635+0dovale@users.noreply.github.com>
2025-08-07 15:43:39 -0300

    Save

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.innerHTML = `
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
+
     <div class="cartao__conteudo__pergunta">
         <p>${pergunta}</p>
     </div>
+
     <div class="cartao__conteudo__resposta">
         <p>${resposta}</p>
     </div>
+
     </div>
     `
 
